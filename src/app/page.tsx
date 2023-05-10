93f512f5328a03648c307766fe26182e06d81db3
isaac <isaacsvianna@gmail.com>
2023-05-11 00:45:06 +0100

    chore: Change name of component Countdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-// import Countdown from './components/Countdown';
-import Logo from './components/Logo';
+import Countdown from './components/countdown';
+import Logo from './components/logo';
 
 export default function Home() {
   return (
     <>
       <main className='flex min-h-screen flex-col items-center justify-center p-8 gap-14 md:p-24'>
         <Logo />
-        {/* <Countdown date='2023-06-01T15:00:00' /> */}
+        <Countdown date='2023-06-01T15:00:00' />
       </main>
     </>
   );
