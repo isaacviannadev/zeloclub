98823b5eaf257c7a76b7d9399fe13ba9caa834fb
isaac <isaacsvianna@gmail.com>
2023-05-11 00:53:23 +0100

    fix: ? 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,9 @@ import Logo from './components/logo';
 
 export default function Home() {
   return (
-    <>
-      <main className='flex min-h-screen flex-col items-center justify-center p-8 gap-14 md:p-24'>
-        <Logo />
-        <Countdown date='2023-06-01T15:00:00' />
-      </main>
-    </>
+    <main className='flex min-h-screen flex-col items-center justify-center p-8 gap-14 md:p-24'>
+      <Logo />
+      <Countdown date='2023-06-01T15:00:00' />
+    </main>
   );
 }
