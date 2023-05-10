cdd36cd2424ac6ba32d24075e6d7e608641702e0
isaac <isaacsvianna@gmail.com>
2023-05-11 00:55:56 +0100

    fix: ? 3

--- src/app/components/Logo/index.tsx
+++ src/app/components/Logo/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Modal from '../modal';
 
 export default function Logo() {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-center p-8 gap-14 md:p-24'>
       <Logo />
-      <Countdown date='2023-06-01T15:00:00' />
+      <Countdown date='2023-06-01T00:00:00' />
     </main>
   );
 }
